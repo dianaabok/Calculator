f4d66a89223f2778300e9f203482c50685f61fe9
diana abok <diana abok>
2017-01-24 21:50:02 +0000

    Commit with subtract operation

--- Calc.java
+++ Calc.java
@@ -5,5 +5,10 @@ public class Calc{
       double two=Double.parseDouble(args[2]);
       System.out.println(one+two);
     }
+	        if(args[0].equals("subtract")){
+      double one=Double.parseDouble(args[1]);
+      double two=Double.parseDouble(args[2]);
+      System.out.println(one-two);
+    }
   }
 }
