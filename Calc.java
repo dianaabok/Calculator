095f90dc2105a3200aab72a46482498ddfc5199e
diana abok <diana abok>
2017-01-24 21:58:02 +0000

    Diana commit with multiply

--- Calc.java
+++ Calc.java
@@ -5,15 +5,20 @@ public class Calc{
       double two=Double.parseDouble(args[2]);
       System.out.println(one+two);
     }
-	        if(args[0].equals("subtract")){
+    if(args[0].equals("subtract")){
       double one=Double.parseDouble(args[1]);
       double two=Double.parseDouble(args[2]);
       System.out.println(one-two);
     }
-		        if(args[0].equals("divide")){
+    if(args[0].equals("divide")){
       double one=Double.parseDouble(args[1]);
       double two=Double.parseDouble(args[2]);
       System.out.println(one/two);
+    }
+    if(args[0].equals("multiply")){
+      double one=Double.parseDouble(args[1]);
+      double two=Double.parseDouble(args[2]);
+      System.out.println(one*two);
     }
   }
 }
