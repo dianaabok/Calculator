94027143b6f23e1a807a549e835525ebe332bfcd
diana abok <diana abok>
2017-01-24 20:41:30 +0000

    our first commit

---
+++ Calc.java
@@ -0,0 +1,9 @@
+public class Calc{
+  public static void main(String[] args){
+    if(args[0].equals("add")){
+      double one=Double.parseDouble(args[1]);
+      double two=Double.parseDouble(args[2]);
+      System.out.println(one+two);
+    }
+  }
+}
